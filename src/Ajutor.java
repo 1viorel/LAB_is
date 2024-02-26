aa8e2897a59c12f04654ab465885657a9a852dc3
Viorel <soituviorel@gmail.com>
2024-02-26 10:46:40 +0200

    Initial comit

---
+++ src/Ajutor.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+
+public class Ajutor{
+    static void afisare() {
+        System.out.println("Salut lume");
+    }
+    static void info() {
+        System.out.println("Proiect: git stuff");
+        System.out.println("Facultatea: AC");
+        System.out.println("Autor: Soitu Viorel");
+    }
+}
